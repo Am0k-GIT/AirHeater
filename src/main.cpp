c7503c5425156ecd355a1586325848597080db08
Am0k-GIT <b1gb0ss@mail.ru>
2021-01-30 10:46:05 +0300

    -

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-//#include <avr/wdt.h>                                                           // Подключаем watchdog
+#include <avr/wdt.h>                                                           // Подключаем watchdog
 
 #define Mode1PIN 5                                                             // PIN управления режимом 1 (0 - выкл, 1 - вкл.)
 #define Mode2PIN 6                                                             // PIN управления режимом 2 (0 - выкл, 1 - вкл.)
@@ -7,6 +7,8 @@
 #define SparkPlugPIN 8                                                         // PIN управления свечей (0 - выкл, 1 - вкл.)
 #define LEDPIN 9                                                               // PIN для подключения светодиода
 #define OverheatSensorPIN A0                                                   // PIN для подключения датчика перегрева (1 - норма, 0 - авария)
+
+//#define WDT_ENABLE                                                           // Включаем WDT (не работает на старых bootloader'ах)
 
 uint8_t timeBlow = 20;                                                         // Время работы 2 режима при запуске
 uint8_t timeSpark = 15;                                                        // Время работы свечей при запуске
@@ -18,7 +20,6 @@ unsigned long startTimer;                                                      /
 
 void startButton();                                                            // Прототип функции
 void changeMode();                                                             // Прототип функции
-void start_func();                                                             // Прототип функции
 
 void setup() {
   pinMode(Mode1PIN, OUTPUT);                                                   // Настраиваем порт для вывода
@@ -27,9 +28,11 @@ void setup() {
   pinMode(SparkPlugPIN, OUTPUT);                                               // Настраиваем порт для вывода
   pinMode(LEDPIN, OUTPUT);                                                     // Настраиваем порт для вывода
   pinMode(OverheatSensorPIN, INPUT_PULLUP);                                    // Настраиваем порт для ввода (включаем внутреннюю подтяжку к +5V)
-  attachInterrupt(0, startButton, RISING);                                     // Запускаем отслеживание прерываний (нажатие кнопки "Старт")
-  attachInterrupt(1, changeMode, RISING);                                      // Запускаем отслеживание прерываний (нажатие кнопки "Режим")
-  //wdt_enable(WDTO_4S);                                                         // Включаем watchdog на 4 секунды
+  attachInterrupt(0, startButton, RISING);                                     // Запускаем отслеживание прерываний (нажатие кнопки "Старт" 0 PIN)
+  attachInterrupt(1, changeMode, RISING);                                      // Запускаем отслеживание прерываний (нажатие кнопки "Режим" 1 PIN)
+  #ifdef WDT_ENABLE
+  wdt_enable(WDTO_4S);                                                         // Включаем watchdog на 4 секунды
+  #endif
 }
 
 //================== Функция для управления внешними подключениями ============
@@ -39,6 +42,33 @@ void IOcontrol (bool Mode1PINstate, bool Mode2PINstate, bool FuelPINstate, bool 
   digitalWrite (FuelPIN, FuelPINstate);
   digitalWrite (SparkPlugPIN, SparkPlugPINstate);
   analogWrite  (LEDPIN, LED_PWM);
+}
+//=============================================================================
+
+//============ Режим запуска ==================================================
+void start_func() {
+  if (flagStartButtonAvailable) {                                            // Если кнопка еще доступна (мы попали в функцию в первый раз)
+    startTimer = millis();                                                   // Записываем время (однократно для запуска)
+    flagStartButtonAvailable = false;                                        // Кнопка запуска недоступна
+  }
+  if ((millis() - startTimer) < (timeBlow * 1000)) {                         // Если прошло меньше времени чем timeBlow
+    IOcontrol (0,1,0,0,50);                                                  // Управляем внешними подключениями
+  }
+  else {                                                                     // Прошло больше времени чем timeBlow
+    if ((millis - startTimer) < ((timeBlow + timeDelay) * 1000)) {           // Прошло меньше времени чем timeBlow + timeDelay
+      IOcontrol (0,0,0,1,50);                                                // Управляем внешними подключениями
+    }
+    else {                                                                   // Прошло больше времени чем timeBlow + timeDelay
+      if ((millis - startTimer) < ((timeBlow + timeSpark) * 1000)) {         // Прошло меньше времени чем timeBlow + timeSpark
+        IOcontrol (1,0,1,1,50);                                              // Управляем внешними подключениями
+      }
+      else {                                                                 // Прошло больше времени чем timeBlow + timeSpark
+        startMode = false;                                                   // Запуск завершен
+        flagStartButtonAvailable = true;                                     // Кнопка запуска доступна
+        workMode = 1;                                                        // Режим работы 1
+      }
+    }
+  }
 }
 //=============================================================================
 
@@ -62,7 +92,9 @@ void loop() {
       break;
     }
   }
-  //wdt_reset();                                                                 // Сбрасываем watchdog 
+  #ifdef WDT_ENABLE
+  wdt_reset();                                                                 // Сбрасываем watchdog 
+  #endif
 }
 
 //============ Нажатие кнопки Старт ===========================================
@@ -97,29 +129,3 @@ void changeMode() {
   }
 }
 //=============================================================================
-
-//============ Режим запуска ==================================================
-void start_func() {
-  if (flagStartButtonAvailable) {                                            // Если кнопка еще доступна (мы попали в функцию в первый раз)
-    startTimer = millis();                                                   // Записываем время (однократно для запуска)
-    flagStartButtonAvailable = false;                                        // Кнопка запуска недоступна
-  }
-  if ((millis() - startTimer) < (timeBlow * 1000)) {                         // Если прошло меньше времени чем timeBlow
-    IOcontrol (0,1,0,0,50);                                                  // Управляем внешними подключениями
-  }
-  else {                                                                     // Прошло больше времени чем timeBlow
-    if ((millis - startTimer) < ((timeBlow + timeDelay) * 1000)) {           // Прошло меньше времени чем timeBlow + timeDelay
-      IOcontrol (0,0,0,1,50);                                                // Управляем внешними подключениями
-    }
-    else {                                                                   // Прошло больше времени чем timeBlow + timeDelay
-      if ((millis - startTimer) < ((timeBlow + timeSpark) * 1000)) {         // Прошло меньше времени чем timeBlow + timeSpark
-        IOcontrol (1,0,1,1,50);                                              // Управляем внешними подключениями
-      }
-      else {                                                                 // Прошло больше времени чем timeBlow + timeSpark
-        startMode = false;                                                   // Запуск завершен
-        flagStartButtonAvailable = true;                                     // Кнопка запуска доступна
-        workMode = 1;                                                        // Режим работы 1
-      }
-    }
-  }
-}
